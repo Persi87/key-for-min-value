c9bb611c74faf505bc74d518e5e1880f3ae432d9
Persi87 <simon_black87@hotmail.co.uk>
2021-07-27 23:43:00 +0100

    commit tree 85117df449fc4c26bed4c9360c7bd95596c30a1b

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+    lowest_value = nil  # default value is given to start the comparison
+    lowest_key = nil
+        name_hash.each do |key, value|
+            if lowest_value == nil || value < lowest_value  #first iteration, lowest_value & lowest_key are nil, therefore...
+                lowest_value = value    #...after the 1st iteration, lowest_value & lowest_key will == the first value & key...
+                lowest_key = key    #...that are in the hash... on the next iteration it will check if value < lowest_value...
+            end         #... and reassign lowest_value if it's true or run the next iteration and repeat the comparison. 
+        end
+    lowest_key
 end
